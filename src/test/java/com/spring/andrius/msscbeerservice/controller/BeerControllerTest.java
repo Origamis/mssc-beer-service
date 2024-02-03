c104900e8096a9046b2a0468a660a2775f331108
andrius-butent <andrius.antipovas@gmail.com>
2024-02-03 17:23:17 +0200

    Unit tests dependencies

--- src/test/java/com/spring/andrius/msscbeerservice/controller/BeerControllerTest.java
+++ src/test/java/com/spring/andrius/msscbeerservice/controller/BeerControllerTest.java
@@ -13,7 +13,7 @@ import java.util.UUID;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
